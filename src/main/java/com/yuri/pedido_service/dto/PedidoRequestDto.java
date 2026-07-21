a22dd600b2d152290543bb38e77c71f02bc4f364
YuriiFreitass <yurifreitas1501@gmail.com>
2026-07-21 14:12:08 -0300

    feat: adiciona descricao e valorTotal no PedidoRequestDto

--- src/main/java/com/yuri/pedido_service/dto/PedidoRequestDto.java
+++ src/main/java/com/yuri/pedido_service/dto/PedidoRequestDto.java
@@ -1,7 +1,11 @@
 package com.yuri.pedido_service.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record PedidoRequestDto(
-		@NotNull int numeroPedido
+		@NotNull int numeroPedido,
+		@NotBlank String descricao,
+		@NotNull @Positive double valorTotal
 ) {}
